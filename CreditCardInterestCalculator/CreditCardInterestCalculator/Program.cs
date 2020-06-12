ac485ebc14f7704a4a3f5322f18f16f7c482e2de
SamSchmitty <sam.schmitty@gmail.com>
2020-06-12 16:05:32 -0500

    Changed to .NET framework to remove files/services not needed

--- CreditCardInterestCalculator/CreditCardInterestCalculator/Program.cs
+++ CreditCardInterestCalculator/CreditCardInterestCalculator/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CreditCardInterestCalculator
 {
@@ -6,7 +10,6 @@ namespace CreditCardInterestCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
